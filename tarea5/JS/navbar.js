b59f6f3edb026294245be6c7a15e9f46f31e9f99
MauroMilan <ing.mauromilan@gmail.com>
2026-06-11 23:14:43 -0600

    Se cambia el fondo de la pagina nosotros y se conecta el navbar

--- tarea5/JS/navbar.js
+++ tarea5/JS/navbar.js
@@ -20,11 +20,11 @@ header.innerHTML = `
     </li>
 
     <li class="nav-item">
-        <a class="nav-link" href="#">Nosotros</a>
+        <a class="nav-link" href="nosotros.html">Nosotros</a>
     </li>
 
     <li class="nav-item">
-        <a class="nav-link" href="#">Contáctanos</a>
+        <a class="nav-link" href="contactanos.html">Contáctanos</a>
     </li>
 </ul>
 `;
